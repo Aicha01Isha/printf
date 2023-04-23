135cae510bce2f71ed344b2e624c837f2062bf1a
bienfati <bienfati061@gmail.com>
2023-04-23 11:26:49 -0700

    try

---
+++ get_width.c
@@ -0,0 +1,37 @@
+
+#include "main.h"
+
+/**
+ * get_width - Determines the width needed for outputting the value
+ * @format: A string that specifies the format for printing the arguments.
+ * @i: A list of parameters that will be printed.
+ * @list: list of parameters 
+ *
+ * Return: width.
+ */
+int get_width(const char *format, int *i, va_list list)
+{
+	int curr_i;
+	int width = 0;
+
+	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+	{
+		if (is_digit(format[curr_i]))
+		{
+			width *= 10;
+			width += format[curr_i] - '0';
+		}
+		else if (format[curr_i] == '*')
+		{
+			curr_i++;
+			width = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
+
+	*i = curr_i - 1;
+
+	return (width);
+}
